3015cb7a67fe31f82e216205680e4e6fd3d8e33e
Иван Сивохин <nigganwd63@mail.ru>
2022-06-02 16:13:05 +0300

    mili modernizaciya

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int bonusScale = 20;
+        int bonusMile = cost /bonusScale;
+        return bonusMile;
+    }
+    }
+
